88091263daee68d5bb770cf52de521d413ee0914
dyalnding <dylanding6464@gmail.com>
2020-09-01 21:13:38 +0000

    new

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 int main(void) {
   double cel, fahr;
-        printf( "Enter temperature in celsius: " );
+        printf( "Enter temperature in celsius: \n" );
         scanf( "%lf", &cel );
         fahr = ( 9.0 / 5.0 ) * cel + 32;
         printf( "%.0f° in Celsius is equivalent to %.1f°\n", cel, fahr );
